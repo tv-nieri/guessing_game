a7b06823b379a667d8f8003f90627c3549d73d7e
Tarcio Vinicius Nieri <tv.nieri@gmail.com>
2022-12-21 19:09:20 -0300

    generating the secret number

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 // This is a guessing game program.
 // 2022-12-21
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("-- GUESS THE NUMBER --");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess:");
 
