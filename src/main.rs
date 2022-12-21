c9e49d8a2d103e533ca467908c096161b7e3adc0
Tarcio Vinicius Nieri <tv.nieri@gmail.com>
2022-12-21 18:32:41 -0300

    getting input from the user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// This is a guessing game program.
+// 2022-12-21
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("-- GUESS THE NUMBER --");
+
+    println!("Please input your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line...");
+
+    println!("You guessed: {}", guess);
 }
