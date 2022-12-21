732f642dab9e4b8808c672ac5b3f9dae510c2453
Tarcio Vinicius Nieri <tv.nieri@gmail.com>
2022-12-21 19:54:49 -0300

    allowing multiple attempts with loop and break when the guess is correct

--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,26 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess:");
+    loop {
+        println!("Please input your guess:");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line...");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line...");
 
-    let guess: u32 = guess.trim().parse().expect("Plase type a number!");
+        let guess: u32 = guess.trim().parse().expect("Plase type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too low!!!"),
-        Ordering::Greater => println!("Too hight!!!"),
-        Ordering::Equal => println!("You win!!!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!!!"),
+            Ordering::Greater => println!("Too hight!!!"),
+            Ordering::Equal => {
+                println!("You win!!!");
+                break;
+            }
+        }
     }
 }
